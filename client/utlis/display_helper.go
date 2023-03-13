776bfe5ab1654977064d84fe8e1c43867f6fc5c3
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2023-03-14 01:41:06 +0800

    added more flights

--- client/utlis/display_helper.go
+++ client/utlis/display_helper.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/jedib0t/go-pretty/text"
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	. "github.com/soulpower11/CZ4013-Project/const"
 )
 
@@ -29,9 +29,9 @@ func DisplayFlightInfo(flightInfos []QueryDepartureTimeResponse) {
 	t.SetOutputMirror(os.Stdout)
 	t.SetStyle(table.StyleColoredDark)
 	t.AppendHeader(table.Row{"Departure Time", "Air Fare", "Seat Availability"})
-	transformer := text.NewTimeTransformer(time.Kitchen, time.Local)
+	timeTransformer := text.NewTimeTransformer(time.Kitchen, time.Local)
 	for _, flightInfo := range flightInfos {
-		t.AppendRow([]interface{}{transformer(flightInfo.DepartureTime), flightInfo.AirFare, flightInfo.SeatAvailability})
+		t.AppendRow([]interface{}{timeTransformer(flightInfo.DepartureTime), flightInfo.AirFare, flightInfo.SeatAvailability})
 	}
 
 	t.Render()
